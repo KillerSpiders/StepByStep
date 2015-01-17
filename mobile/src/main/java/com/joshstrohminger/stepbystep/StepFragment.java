d39d94819c144e672350562d9563d6aedfd186e2
Josh Strohminger <jdst8-hackathon@yahoo.com>
2015-01-17 10:16:07 -0800

    added list to step fragment

--- mobile/src/main/java/com/joshstrohminger/stepbystep/StepFragment.java
+++ mobile/src/main/java/com/joshstrohminger/stepbystep/StepFragment.java
@@ -6,11 +6,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.Arrays;
 
 public class StepFragment extends Fragment {
 
     private static final String ARG_STEPS_ID = "arg_steps_id";
+
+    private ListView listView;
+    private int stepsId;
+    private String title = "Please select some steps";
+    private String subtitle = "so they'll show up here";
+
+    private String[] instructions = new String[] {};
 
     public StepFragment() {
     }
@@ -25,10 +36,24 @@ public class StepFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_step, container, false);
-        ((TextView)rootView.findViewById(R.id.textView)).setText("steps id: " + getArguments().getInt(ARG_STEPS_ID));
+        stepsId = getArguments().getInt(ARG_STEPS_ID);
+        if(stepsId > 0) {
+            String[] steps = getResources().getStringArray(stepsId);
+            if (steps.length >= 3) {
+                title = steps[0];
+                subtitle = steps[1];
+                instructions = Arrays.copyOfRange(steps, 2, steps.length);
+            }
+        } else {
+            rootView.findViewById(R.id.controlPanel).setVisibility(View.INVISIBLE);
+        }
+        listView = (ListView) rootView.findViewById(R.id.listView);
+        ((TextView) rootView.findViewById(R.id.textViewTitle)).setText(title);
+        ((TextView) rootView.findViewById(R.id.textViewSubtitle)).setText(subtitle);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_single_choice, android.R.id.text1, instructions);
+        listView.setAdapter(adapter);
         return rootView;
     }
 
